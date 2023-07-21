8eae6ccbba37126a229b12ba055e83883661ce1a
Ikhsanuddin Syamsuri <mail.ikhsanuddin@gmail.com>
2023-07-22 02:19:47 +0700

    📖 Change wording

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,7 +15,7 @@ export default function Blog() {
             From the blog
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600 dark:text-neutral-400">
-            Learn how to grow your business with our expert advice.
+            The programming journey.
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
